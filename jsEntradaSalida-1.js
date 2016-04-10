cefc73d01c92ca511f978cda99e888eaa8cc1225
Alejandro <alejandro.pratessi@gmail.com>
2016-04-10 18:05:05 -0300

    Ejercicio 1 - Mensaje enviado OK

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,7 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+
+	  document.getElementById("miMensaje").innerHTML = "Esto funciona correctamente" ;
+}
+
